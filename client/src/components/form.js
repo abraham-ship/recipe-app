571d317b284cabeec8623adfd83de5a6afefa6be
unknown <abuzsein3@gmail.com>
2024-06-05 23:02:32 +0300

    added form for authentication and a simple navBar

---
+++ client/src/components/form.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Form = ({ email, setEmail, password, setPassword, label, onSubmit }) => {
+    return (
+        <div className="auth-container">
+            <form onSubmit={onSubmit}>
+                <h2>{label}</h2>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+                <button type="submit">{label}</button>
+            </form>
+        </div>
+    );
+};
+
+export default Form;
---
+++ client/src/components/navBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import useLogout from '../utils/logout';
+import '../styles/navBar.css';
+
+const Navbar = ({ isLoggedIn }) => {
+    const [cookies, , removeCookie] = useCookies(['access_token']);
+    const navigate = useNavigate();
+    const logout = () => {
+        removeCookie('access_token', { path: '/' });
+        localStorage.removeItem('userID');
+        navigate('/auth');
+    };
+
+    return (
+        <div className="navbar">
+            <Link to="/">Home</Link>
+            {isLoggedIn && <Link to="/create-recipe">Create Recipe</Link>}
+            {isLoggedIn && <Link to="/saved-recipes">Saved Recipes</Link>}
+            {!isLoggedIn ? (
+                <Link to="/auth">Login/Register</Link>
+            ) : (
+                <button onClick={logout}>Logout</button>
+            )}
+        </div>
+    );
+};
+
+export default Navbar;
