6766971320b1191e9fdb8d11944b75b069abfa8c
unknown <abuzsein3@gmail.com>
2024-06-11 07:42:35 +0300

    updated navbar

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
-import useLogout from '../utils/logout';
 import '../styles/navBar.css';
 
 const Navbar = ({ isLoggedIn }) => {
